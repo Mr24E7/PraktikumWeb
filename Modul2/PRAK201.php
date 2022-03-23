a994117c07378641ccc6a02b9227e8bbe06e0fb2
Mr24 <101628346+Mr24E7@users.noreply.github.com>
2022-03-23 08:12:14 +0800

    Add files via upload

---
+++ Modul2/PRAK201.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head> 
+</head>
+<body>
+<div class="box">
+        <h3>Masukkan Hasil Yang Diinginkan</h3>
+        <form action="" method="post">           
+            <input type="text" name="a"><br>
+            <input type="text" name="b"><br>
+            <input type="text" name="c"><br>
+            <input type="submit" name="submit" value="Urutkan">
+        </form>
+ 
+        <?php  
+        if(isset($_POST['submit'])){
+            $a=$_POST['a'];
+            $b=$_POST['b'];
+            $c=$_POST['c'];
+            $angka=array($a,$b,$c); 	
+			
+            sort($angka);  
+            echo "</br>";
+            echo "<div class='hasil'> HASIL : ";
+            
+            $jumlah=count($angka);  
+            for($x=0;$x<$jumlah;$x++)  
+            {  
+                echo $angka[$x];  
+                echo " ";  
+            }  	
+            echo "</div>";
+ 
+        }
+        ?>  
+    </div>
+</body>
+</html>
